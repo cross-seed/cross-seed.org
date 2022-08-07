1e690d49e1644aff1341cf9610fbaab1bbdec014
Michael Goodnow <mmgoodnow@gmail.com>
2022-08-07 05:08:45 -0400

    Update cd.yml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,12 @@ const config = {
 	onBrokenMarkdownLinks: "warn",
 	favicon: "img/favicon.ico",
 
-	// // GitHub pages deployment config.
-	// // If you aren't using GitHub pages, you don't need these.
-	// organizationName: "facebook", // Usually your GitHub org/user name.
-	// projectName: "docusaurus", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "cross-seed", // Usually your GitHub org/user name.
+	projectName: "cross-seed.org", // Usually your repo name.
+	deploymentBranch: "dist",
+	trailingSlash: false,
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
